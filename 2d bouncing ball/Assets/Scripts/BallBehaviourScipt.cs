47a574029ac2c4d18e303b268360ac480b621153
Gazal Patel <gazal105patel@gmail.com>
2017-04-19 23:43:50 -0700

    Add files via upload

---
+++ 2d bouncing ball/Assets/Scripts/BallBehaviourScipt.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBehaviourScipt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // This Function appies physic code on game object
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+       // float moveVerticle = Input.GetAxis("Verticle");
+
+        //Vector3 movement = new Vector3(moveHorizontal, moveVerticle, 0.0f);
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        rigidbody.AddForce(movement);
+        Vec;
+
+    }
+}
